2a0f6daa68181aea258d4d7c93144d3ed277b3fe
ForgedVision <nazar.sh@gmail.com>
2013-08-18 22:18:33 -0400

    some seeds file building tools and tab separated word list

---
+++ lib/tools/seeds.rb
@@ -0,0 +1,367 @@
+Card.delete_all
+Card.create!(
+  group_id: '1',
+  term: 'vise',
+  meaning: 'vice; tool for holding work in place; clamping device',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vision',
+  meaning: 'eyesight; foresight; mental image produced by the imagination; experience of seeing the supernatural',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'visionary',
+  meaning: 'produced by imagination; fanciful; mystical; showing foresight; N: one having foresight; one given to speculative impractical ideas',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vital',
+  meaning: 'full of life; animated; vibrant and lively; critical; of great importance; necessary to stay alive; of life; living; breathing; N. vitality; V. vitalize',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vitiate',
+  meaning: 'spoil the effect of; make inoperative; corrupt morally',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vitreous',
+  meaning: 'of glass; pertaining to or resembling glass; V. vitrify: change into glass; CF. petrify',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vitriolic',
+  meaning: 'causing sharp pain to the mind; caustic; corrosive; sarcastic; of vitriol; N. vitriol: sulphuric acid (which burns flesh deeply); bitterly abusive expression; caustic expression; CF. glass',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vituperative',
+  meaning: 'abusive; scolding; V. vituperate: berate; scold; rail against abusively',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vivacious',
+  meaning: 'lively or animated; sprightly',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vivid',
+  meaning: '(of light or color) bright and distinct; evoking lifelike mental images; Ex. vivid red hair/description',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vivisection',
+  meaning: 'act of dissecting living animals',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vixen',
+  meaning: 'female fox; ill-tempered woman; CF. shrew',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vociferous',
+  meaning: 'clamorous; noisy; V. vociferate: cry out loudly (when complaining)',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vogue',
+  meaning: 'popular fashion; Ex. Jeans became the vogue.',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'volatile',
+  meaning: 'changeable; of a quickly changing nature (as of temper); mercurial; tending to violence; evaporating rapidly; Ex. volatile character/situation in the street',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'volition',
+  meaning: 'act of using one\'s will; act of making a conscious choice; Ex. She selected this dress of her own volition.',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'volley',
+  meaning: 'simultaneous discharge of a number of shots; V.',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'voluble',
+  meaning: 'fluent; talkative; glib; N. volubility',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'voluminous',
+  meaning: 'having great volume (as of a garment or container); bulky; large',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'voluptuous',
+  meaning: 'sensual; of or giving sensual pleasure; indulging in sensual pleasures; Ex. voluptuous lines; Ex. voluptuous life of the Romans; N. voluptuary: voluptuous person',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'voodoo',
+  meaning: 'religion practiced chiefly in Haiti',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'voracious',
+  meaning: 'ravenous; eating large quantities of food; exceedingly eager; insatiable; Ex. voracious animal/reader',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vortex',
+  meaning: 'whirlwind; whirlpool; center of turbulence; predicament into which one is inexorably plunged',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vouch',
+  meaning: 'give a personal guarantee; Ex. I can vouch for his integrity; N. voucher',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vouchsafe',
+  meaning: 'grant condescendingly; guarantee; Ex. vouchsafe your fair return on your investment',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'voyeur',
+  meaning: 'Peeping Tom; person who derives sexual gratification from observing the sexual acts of others',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vulgar',
+  meaning: 'of the common people; deficient in refinement; not refined; coarse; Ex. vulgar display of wealth; N. vulgarism: vulgarity; crudely indecent word; CF. vulgarian: vulgar person; boor; lout',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vulnerable',
+  meaning: 'susceptible to wounds or attack; N. vulnerability',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vulpine',
+  meaning: 'like a fox; crafty',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vulture',
+  meaning: 'carrion-eating birds',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'vying',
+  meaning: 'contending; CF. vie',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'wade',
+  meaning: 'walk through a substance, such as water, that impedes movement',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'waffle',
+  meaning: 'speak equivocally about an issue; N.',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'waft',
+  meaning: 'move gently (in air or in seas) by wind or waves; Ex. leaves wafting past the window',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'wag',
+  meaning: 'shake repeatedly from side to side; Ex. The dog wagged its tail; N: humorous person; wit',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'wage',
+  meaning: 'begin and continue (a war)',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'waggish',
+  meaning: 'humorous; mischievous; tricky',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'waif',
+  meaning: 'homeless child or animal; Ex. waifs and strays',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'waive',
+  meaning: 'give up temporarily; yield; N. waiver: waiving a right or claim; document that waives a right or claim',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'wake',
+  meaning: 'trail of ship or other object through water; path of something that has gone before; Ex. hunger followed in the wake of the war',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'wallow',
+  meaning: 'roll in mud; indulge in; (of a ship) roll in a rough sea; become helpless; Ex. wallow in the mud/luxury',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'wan',
+  meaning: 'having a pale or sickly color; pallid',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'wanderlust',
+  meaning: 'strong longing to travel',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'wane',
+  meaning: 'decrease in size or strength (after being full); grow gradually to an end; Ex. The moon waxes and wanes every month; N.',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'wangle',
+  meaning: 'achieve by cleverness or trick; wiggle out; fake; Ex. She tried to wangle an invitation to the party.',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'wanton',
+  meaning: 'unrestrained; gratuitously cruel; willfully malicious; unchaste; sexually improper; promiscuous; Ex. wanton spending/killing; CF. having no just cause',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'warble',
+  meaning: '(of a bird) sing; babble; N.',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'ward',
+  meaning: 'administrative division of a city; division in a hospital or prison; incompetent person placed under the protection of a guardian; V: guard; ward off: avert',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'warp',
+  meaning: 'twist out of shape; N.',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'warrant',
+  meaning: 'justification; written order that serves as authorization (esp. a judicial writ); Ex. search/death warrant; V: justify; guarantee',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'warranted',
+  meaning: 'justified; authorized',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'warranty',
+  meaning: 'guarantee; assurance by seller',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'warren',
+  meaning: 'tunnels in which rabbits live; overcrowded living area; crowded conditions in which people live',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'wary',
+  meaning: 'very cautious; watchful',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'wastrel',
+  meaning: 'waster; profligate',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'waver',
+  meaning: 'move or swing back and forth; be uncertain or unsteady in decision or movement; Ex. wavering between accepting and refusing',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'wax',
+  meaning: 'increase gradually (as the moon); grow',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'waylay',
+  meaning: 'ambush; lie in wait for and attack',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'wean',
+  meaning: 'accustom a baby not to nurse; accustom (the young of a mammal) to take nourishment other than by suckling; give up a cherished activity; cause to gradually leave (an interest or habit); Ex. wean oneself from cigarettes',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'weary',
+  meaning: 'tired after long work; V.',
+  done: 'false'
+)
+Card.create!(
+  group_id: '1',
+  term: 'weather',
+  meaning: 'pass safely through (a storm ',
+  done: 'false'
+)
